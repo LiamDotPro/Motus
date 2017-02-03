36b34ace3f18824cc248786fd913c534567ddc2a
Liam Read <Liam@liam.pro>
2017-02-03 08:01:12 +0000

    Changes to try and make webpack work

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 ﻿const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
     template: './client/index.html',
     filename: 'index.html',
@@ -14,7 +15,7 @@ module.exports = {
     module: {
         loaders: [
             { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-            { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ }
+            { test: /\.jsx?$/, loaders: ['react-hot-loader', 'babel-loader'], exclude: /node_modules/ }
         ]
     },
     plugins: [HtmlWebpackPluginConfig]
