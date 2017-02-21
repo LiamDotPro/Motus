042ad61c931b0e3ff466d7644187a5c771973848
Liam Read <Liam@liam.pro>
2017-02-21 13:20:35 +0000

    react redux compliation

---
+++ src/components/NewsPane.js
@@ -0,0 +1,14 @@
+﻿import React from "react";
+import "../stylesheets/main.scss";
+
+export default class NewsPane extends React.Component {
+    render() {
+        return (
+         <div className="col-xs-9 home-pane-main nopad">
+            <div className="news-hub">
+
+            </div>
+        </div>
+        );
+    }
+};
